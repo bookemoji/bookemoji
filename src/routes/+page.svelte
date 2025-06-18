e2e6091b98ea2c903633390f9f03120ed48e8b08
Rory III <rory.duncan@worktrucksolutions.com>
2025-06-18 00:21:35 -0700

    Run fmt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import { base } from "$app/paths";
-
+  import { base } from "$app/paths";
 </script>
+
 <ul>
   <li>
     <a href={`${base}/books`}>Example Component Library</a>
--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
  * @type {import('@sveltejs/kit').Config}
  */
 const config = {
-  preprocess: [vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
     // mdsvex()
-
   ],
   kit: {
     adapter: adapter(),
