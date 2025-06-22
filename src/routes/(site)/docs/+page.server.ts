cd5878550ce92469bcae8ca1f5d70e71a4b3dbad
Rory Duncan <robust.rory@gmail.com>
2025-06-22 11:48:12 -0700

    Resolve issue with base

--- src/routes/(site)/docs/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types.js";
-import { base } from "$app/paths";
-
-export const load: PageServerLoad = () => {
-  redirect(307, `${base}/docs/getting-started`);
-};
