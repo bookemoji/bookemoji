1c233270d21bb665353f5f0d31ff662801feb882
Rory III <rory.duncan@worktrucksolutions.com>
2025-06-23 08:58:49 -0700

    base missing. where's my type checker at

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,20 @@
   <h2>a SvelteKit Component Library</h2>
 </div>
 
+<p>
+  📕 Bookemoji is a component library for svelte. Rethought to suite the needs of a sveltekit
+  application.
+</p>
+
+<h3>Vision</h3>
+<ul>
+  <li>It integrates tightly with your sveltekit app. — deploys with your application</li>
+  <li>It is a set of unstyled components, yours to own and style freely</li>
+  <li>No JSX, no obscure errors, just straight svelte and sveltekit.</li>
+  <li>Low Dependencies</li>
+  <li>Very little magic—just enough for some sorcerers to wield if astute to learn</li>
+</ul>
+
 <style>
   .hero {
     height: calc(90vh - 2rem);
