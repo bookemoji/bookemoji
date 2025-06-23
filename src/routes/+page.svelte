92d7dca699eb4a4149eb5208a00c1ac35d1dbd16
Rory of the moon <robust.rory@gmail.com>
2025-06-23 09:22:38 -0700

    More content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,28 @@
   <h2>a SvelteKit Component Library</h2>
 </div>
 
-<p>
-  📕 Bookemoji is a component library for svelte. Rethought to suite the needs of a sveltekit
-  application.
-</p>
+<div class="side-by-side">
+  <div>
+    <h3>What is it</h3>
+    <p>
+      📕 Bookemoji is a component library for svelte. Rethought to suite the needs of a sveltekit
+      application.
+    </p>
+  </div>
 
-<h3>Vision</h3>
-<ul>
-  <li>It integrates tightly with your sveltekit app. — deploys with your application</li>
-  <li>It is a set of unstyled components, yours to own and style freely</li>
-  <li>No JSX, no obscure errors, just straight svelte and sveltekit.</li>
-  <li>Low Dependencies</li>
-  <li>Very little magic—just enough for some sorcerers to wield if astute to learn</li>
-</ul>
+  <div>
+    <h3>Vision</h3>
+    <aside class="subtitle">initially vision, but later features</aside>
+    <ul class="book-list">
+      <li>Tight integration with sveltekit</li>
+      <li>Support for Svelte 4.x and 5.x</li>
+      <li>unstyled components, yours style freely</li>
+      <li>No JSX, no obscure errors <br /><i>just svelte and sveltekit.</i></li>
+      <li>Low Dependencies</li>
+      <li>Very little magic — enough for sorcerers to wield</li>
+    </ul>
+  </div>
+</div>
 
 <style>
   .hero {
@@ -29,5 +38,46 @@
 
   .hero :where(h1, h2) {
     margin: 0;
+  }
+
+  .side-by-side {
+    margin: 2rem 0;
+    padding-bottom: 20vh;
+  }
+
+  .subtitle {
+    margin-top: -2.5em;
+    font-style: italic;
+    font-size: 1rem;
+    color: var(--gray-6);
+    margin-bottom: 1rem;
+  }
+
+  @media screen and (min-width: 60rem) {
+    .side-by-side {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+
+  .book-list {
+    padding: 0 0 0 1rem;
+  }
+
+  .book-list li:nth-of-type(4n) {
+    list-style-type: "📕";
+  }
+  .book-list li:nth-of-type(4n - 1) {
+    list-style-type: "📗";
+  }
+  .book-list li:nth-of-type(4n - 2) {
+    list-style-type: "📘";
+  }
+  .book-list li:nth-of-type(4n - 3) {
+    list-style-type: "📙";
+  }
+  .book-list li:nth-of-type(5n) {
+    list-style-type: "📔";
   }
 </style>
