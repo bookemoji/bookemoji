5d96d353c420bcde57c075335ed2936513cbe920
Rory Duncan <robust.rory@gmail.com>
2025-06-22 20:07:02 -0700

    Cleanup

--- src/routes/(example)/+layout.svelte
+++ src/routes/(example)/+layout.svelte
@@ -1,16 +1,7 @@
 <script>
-  import { base } from "$app/paths";
+  import Header from "../Header.svelte";
 </script>
 
-<div class="back-ribbon">
-  <a href={`${base}/`}>Go Back</a>
-</div>
+<Header />
 
 <slot />
-
-<style>
-  .back-ribbon {
-    padding: 0.5rem 2rem;
-    border-bottom: 1px solid var(--surface-2);
-  }
-</style>
--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -8,24 +8,8 @@
 
   import Header from "../Header.svelte";
   import type { LayoutData } from "./$types.js";
-  import { base } from "$app/paths";
   export let data: LayoutData;
 </script>
-
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Jua&text=BookEmoji&display=swap"
-    rel="stylesheet"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
-    rel="stylesheet"
-  />
-
-  <link id="common-site-styles" rel="stylesheet" href={`${base}/site-and-docs.css`} />
-</svelte:head>
 
 <Header />
 
