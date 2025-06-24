15558a7e113ac964021b17761763e120da5d4ce9
Rory of the moon <robust.rory@gmail.com>
2025-06-23 23:26:22 -0700

    Configure initial exports

---
+++ src/lib/components/v4/index.ts
@@ -0,0 +1,8 @@
+export { default as Book } from "./Book.svelte";
+export { default as Controls } from "./Controls.svelte";
+export { default as Story } from "./Story.svelte";
+export { default as StoryList } from "./StoryList.svelte";
+
+// i doubt these need to be public api :thinking:
+// export { default as Isolate } from "./Isolate.svelte";
+// export { default as Variant } from "./Variant.svelte";
