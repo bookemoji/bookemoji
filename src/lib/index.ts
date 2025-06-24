59c9d1c0c02ad3ef5ba3dfded194a3842ef040e4
Rory III <rory.duncan@worktrucksolutions.com>
2025-06-23 23:26:22 -0700

    Configure initial exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // Reexport your entry components here
+export * from "./book-emoji.js";
+export * from "./loaders.js";
+export * from "./entry-generators.js";
+export * from "./components/v4/index.js";
