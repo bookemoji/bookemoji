ee41497080b1e3a3949a7ea2151b4fcc48e5c9d7
Rory Duncan <robust.rory@gmail.com>
2025-06-24 14:03:50 -0700

    Init create functionality

--- packages/create-bookemoji/index.ts
+++ packages/create-bookemoji/index.ts
@@ -1,3 +1,35 @@
 import degit from "degit";
+import { intro, outro, text } from "@clack/prompts";
 
-degit;
+async function main() {
+  intro(`create-bookemoji`);
+  // https://github.com/bookemoji/template
+  // https://github.com/bookemoji/bookemoji
+  const TEMPLATE_REPO: string = "https://github.com/bookemoji/template";
+
+  const meaning = await text({
+    message: "What is the meaning of life?",
+    placeholder: "Not sure",
+    initialValue: "42",
+    validate(value: string) {
+      if (value.length === 0) return `Value is required!`;
+    },
+  });
+  const emitter = degit(TEMPLATE_REPO, {
+    cache: true,
+    force: true,
+    verbose: true,
+  });
+
+  emitter.on("info", (info) => {
+    console.log(info.message);
+  });
+
+  emitter.clone("path/to/dest").then(() => {
+    console.log("done");
+  });
+
+  outro(`You're all set!`);
+}
+
+main();
