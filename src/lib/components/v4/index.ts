73094f4707740fc282861b8ac4090421ec43643d
Rory Duncan <robust.rory@gmail.com>
2025-07-03 17:28:32 -0700

    Ensure require imports

--- src/lib/components/v4/index.ts
+++ src/lib/components/v4/index.ts
@@ -3,6 +3,5 @@ export { default as Controls } from "./Controls.svelte";
 export { default as Story } from "./Story.svelte";
 export { default as StoryList } from "./StoryList.svelte";
 
-// i doubt these need to be public api :thinking:
-// export { default as Isolate } from "./Isolate.svelte";
-// export { default as Variant } from "./Variant.svelte";
+export { default as Isolate } from "./Isolate.svelte";
+export { default as Variant } from "./Variant.svelte";
