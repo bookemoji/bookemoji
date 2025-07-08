283d2c140f1103213a59e951013ada2d3220395d
Rory of the moon <robust.rory@gmail.com>
2025-07-08 16:51:22 -0700

    Add classes for Story

---
+++ src/routes/(example)/books/stories/WebsiteHero.book.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Hero from "$lib/website/components/Hero.svelte";
+  import { defineMeta } from "$lib/book-emoji.js";
+  import Story from "$lib/components/v4/Story.svelte";
+  import Controls from "$lib/components/v4/Controls.svelte";
+
+  defineMeta<typeof Hero>({
+    component: Hero,
+    args: {
+      size: 20,
+    },
+    argTypes: {
+      size: { type: "text" },
+    },
+  });
+</script>
+
+<Story name="Basic" of={Hero} class={"minimal"} let:args>
+  <div style:position="relative">
+    <Hero {...args} />
+  </div>
+</Story>
+
+<Controls story="Basic" of={Hero} />
