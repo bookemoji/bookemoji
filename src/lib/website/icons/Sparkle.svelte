af4194691a84e40ff15dec7bbaee7c81dbb13dbc
Rory of the moon <robust.rory@gmail.com>
2025-07-10 15:01:54 -0700

    Add sparkles

---
+++ src/lib/website/icons/Sparkle.svelte
@@ -0,0 +1,18 @@
+<svelte:options namespace="svg" />
+
+<script lang="ts">
+  export let scale: number = 1;
+  export let width: number = 105;
+  export let height: number = 125;
+
+  var yellowIndex = Math.floor(Math.random() * 5);
+  let fill = `var(--yellow-${yellowIndex})`;
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-5.0 -10.0 110.0 135.0" width={width * scale} height={height * scale}>
+  <path
+    style:fill
+    d="m18.008 49.34c5.9961 2.9453 21.324 0.74219 28.609 3.5742l3.1133 29.586 3.1914-29.859 29.066-2.9688c-3.5391-1.6602-22.914-2.6445-28.531-3.3594-1.8555-3.3047-2.9531-23.355-3.3477-28.812-2.332 4.7539-1.9609 25.5-3.5938 28.105-1.1758 1.875-24.621 3.0273-28.508 3.7344z"
+    fill-rule="evenodd"
+  />
+</svg>
--- src/routes/(example)/books/+layout.svelte
+++ src/routes/(example)/books/+layout.svelte
@@ -4,15 +4,47 @@
   import StoryList from "$lib/components/v4/StoryList.svelte";
   import Collapsible from "$lib/website/components/Collapsible.svelte";
   import { page } from "$app/stores";
+  import { browser } from "$app/environment";
+  import { wait } from "$lib/website/renderer-utils.js";
+
+  import Sparkle from "$lib/website/icons/Sparkle.svelte";
   export let data: LayoutData;
 
   let embellished: boolean = false;
   let animating: boolean = false;
+  let sparkles: (readonly [x: number, y: number, duration: number, delay: number, finalScale: number])[] = [];
+  let sidebarRef: HTMLElement;
+
+  $: makeItSparkle(embellished);
+
+  async function makeItSparkle(_embellished: boolean) {
+    if (browser && _embellished && sparkles.length === 0) {
+      const total = 50 + Math.floor(Math.random() * 50);
+      const bounds = sidebarRef.getBoundingClientRect();
+
+      const animations = Array.from({ length: total }, (i) => {
+        const duration: number = 500 + Math.floor(Math.random() * 2000);
+        const delay = Math.random() > 0.8 ? Math.random() * 600 : 0;
+        const coord = [
+          -0.5 + Math.floor(Math.random() * bounds.width * 2),
+          -0.5 + Math.floor(Math.random() * bounds.height * 1.5),
+          duration,
+          delay,
+          0.2 + Math.random() * 0.6,
+        ] as const;
+        sparkles = [...sparkles, coord];
+        return wait(duration);
+      });
+
+      await Promise.allSettled(animations);
+      sparkles = [];
+    }
+  }
 </script>
 
 <Book base={data.base} books={data.books}>
   <div class="book-layout" class:embellished class:animating on:animationstart={() => (animating = true)} on:animationend={() => (animating = false)}>
-    <div class="sidebar">
+    <div class="sidebar" bind:this={sidebarRef} class:sparkling={sparkles.length > 0}>
       {#if embellished}
         <div class="book-sidebar">
           <div class="brand-header">
@@ -62,10 +94,18 @@
   </div>
 </Book>
 
+<div class="sparkle-overlay" class:active={sparkles.length > 0}>
+  {#each sparkles as [x, y, duration, delay, finalScale]}
+    <div class="sparkle" style:left={`${x}px`} style:top={`${y}px`} style:animation-delay={`${delay}ms`} style:animation-duration={`${duration}ms`}>
+      <Sparkle scale={finalScale} />
+    </div>
+  {/each}
+</div>
+
 <style>
   .book-layout {
     display: grid;
-    grid-template-columns: 13rem 1fr;
+    grid-template-columns: 18rem 1fr;
     min-height: 100vh;
     overflow: hidden;
     border-top: 1px solid var(--surface-2);
@@ -73,10 +113,26 @@
 
   .sidebar {
     height: 100%;
+    position: relative;
     display: flex;
     flex-direction: column;
     border-right: 1px solid var(--surface-2);
     background: var(--surface-1);
+    transition: 0.1s ease-in filter;
+    filter: blur(0) saturate(1) brightness(1);
+  }
+
+  @media (prefers-color-scheme: light) {
+    .sidebar.sparkling {
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .sidebar.sparkling {
+      filter: blur(0) saturate(4) brightness(2.5);
+      box-shadow: 0px 0px 40px var(--surface-3);
+      transition: 0.8s ease-in all;
+    }
   }
 
   .book-layout.embellished {
@@ -156,5 +212,60 @@
 
   :where(.book-title, .variant-title).active {
     --link: var(--pink-6);
+  }
+
+  .sparkle-overlay {
+    position: absolute;
+    font-size: 1em;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    display: none;
+    pointer-events: none;
+  }
+
+  .sparkle {
+    font-size: 2rem;
+    position: fixed;
+    transition: ease-in-out all;
+  }
+
+  .sparkle-overlay.active {
+    display: block;
+    background-color: rgba(255, 255, 255, 0.05);
+  }
+
+  @keyframes sparkle-appear {
+    0% {
+      opacity: 0;
+      transform: scale(0, 0);
+    }
+    20% {
+      opacity: 0;
+      transform: scale(0.7, 1.2);
+    }
+    30% {
+      transform: scale(1.1, 0.8);
+    }
+    40% {
+      transform: scale(0.9, 1.05);
+    }
+    50% {
+      transform: scale(1.05, 0.95);
+    }
+    60% {
+      opacity: 1;
+      transform: scale(1, 1);
+    }
+
+    100% {
+      opacity: 0;
+      transform: scale(0, 0);
+    }
+  }
+
+  .sparkle {
+    animation: sparkle-appear 1s cubic-bezier(0.5, 1.5, 0.5, 1) both;
   }
 </style>
