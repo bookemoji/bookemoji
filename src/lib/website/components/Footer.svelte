e0183485e776285d01a9ff6d81e29bb18d0ff3f9
Rory III <rory.duncan@worktrucksolutions.com>
2025-07-10 15:01:54 -0700

    Add sparkles

--- src/lib/website/components/Footer.svelte
+++ src/lib/website/components/Footer.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+{#if $page.url.pathname.startsWith("/books")}
+  <div class="page-footer">
+    <p><a href="https://thenounproject.com/icon/sparkle-star-7309925/" target="_blank">Sparkle icon Created by Spacenobyl from Noun Project</a></p>
+  </div>
+{/if}
 <footer class="footer">
   <div class="brand-font">Built with 😻</div>
   <div class="link-list">
@@ -7,18 +16,26 @@
 </footer>
 
 <style>
-  .footer {
+  .footer,
+  .page-footer {
+    display: flex;
     border-top: 1px solid var(--surface-2);
-    padding: 2rem 1rem;
+    padding: 1rem;
+    align-items: center;
+  }
+
+  .footer {
     display: flex;
     gap: 1rem;
     justify-content: space-evenly;
-    align-items: center;
+    padding: 2rem 1rem;
+  }
+
+  .page-footer {
+    justify-content: center;
+    --link: var(--stone-8);
   }
   .link-list {
     display: grid;
-  }
-
-  .link-list a {
   }
 </style>
