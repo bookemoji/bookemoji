28eed6379d32bdf482ad2aeb81079b4fc6b38d14
Rory of the moon <robust.rory@gmail.com>
2025-07-27 00:05:28 -0700

    Add blurb about doing better, being better

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,3 +1,11 @@
 <h1>Docs</h1>
 
 Use the sidebar to start your journey.
+
+<h3>Help us make it better</h3>
+<p>
+  <span class="brand">bookemoji</span> is in active development, thus the docs could be <em>much better</em>!
+  <br />
+  If you find issues—or require more clarity — <a href="https://github.com/bookemoji/bookemoji/issues" target="_blank">open an issue on GitHub</a> or contribute
+  a Pull Request.
+</p>
