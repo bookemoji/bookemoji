aba56e08ae802b3b4b26d382cba25ce6097f706d
Rory Duncan <robust.rory@gmail.com>
2025-10-25 11:28:19 -0700

    Add graphics, remove roadmap

--- src/routes/roadmap/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import TwoColumnLayout from "../TwoColumnLayout.svelte";
-  import Roadmap from "./roadmap.mdx";
-</script>
-
-<TwoColumnLayout>
-  <Roadmap />
-</TwoColumnLayout>
